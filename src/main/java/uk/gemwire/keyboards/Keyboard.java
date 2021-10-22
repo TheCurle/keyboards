275be25cb950c4d7a3a47046868b291d6c10d1fe
Curle <curle@gemwire.uk>
2021-10-22 05:09:29 +0100

    Initial commit

---
+++ src/main/java/uk/gemwire/keyboards/Keyboard.java
@@ -0,0 +1,100 @@
+package uk.gemwire.keyboards;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.protocol.game.ServerboundChatPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import javax.annotation.Nullable;
+
+/**
+ * A simple rotateable block that messes with your chat if a cat sits on it.
+ *
+ * @author Curle
+ */
+public class Keyboard extends Block {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    protected static final VoxelShape EAST_AABB = Shapes.join(Shapes.empty(), Shapes.box(0.5625, 0, 0, 1, 0.1875, 1), BooleanOp.OR);
+    protected static final VoxelShape WEST_AABB = Shapes.join(Shapes.empty(), Shapes.box(0, 0, 0, 0.4375, 0.1875, 1), BooleanOp.OR);
+    protected static final VoxelShape SOUTH_AABB = Shapes.join(Shapes.empty(), Shapes.box(0, 0, 0.5625, 1, 0.1875, 1), BooleanOp.OR);
+    protected static final VoxelShape NORTH_AABB = Shapes.join(Shapes.empty(), Shapes.box(0, 0, 0, 1, 0.1875, 0.4375), BooleanOp.OR);
+
+    public Keyboard() {
+        super(Properties.of(Material.STONE).noOcclusion());
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, LevelAccessor world, BlockPos pos, Rotation direction) {
+        return state.setValue(FACING, direction.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter p_60556_, BlockPos p_60557_, CollisionContext p_60558_) {
+        return switch (state.getValue(FACING)) {
+            case SOUTH -> SOUTH_AABB;
+            case WEST -> WEST_AABB;
+            case EAST -> EAST_AABB;
+            default -> NORTH_AABB;
+        };
+    }
+
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
+        super.entityInside(state, level, pos, entity);
+        // On the client..
+        // TODO
+
+        // If there's a cat above us
+        if (!(entity instanceof Cat cat)) return;
+        if (!(pos.equals(new BlockPos(cat.position())))) return;
+        // With a Player owner
+        LivingEntity owner = cat.getOwner();
+        if (!(owner instanceof ServerPlayer player)) return;
+
+        // And the player has chat enabled
+        //if (!(Minecraft.getInstance().getChatStatus() == Minecraft.ChatStatus.ENABLED)) return;
+
+        // And the player isn't currently in the chat box
+        //if (Minecraft.getInstance().screen instanceof ChatScreen) return;
+
+        // And the user is particularly unlucky
+        if (level.getRandom().nextInt(10000) > 2) return;
+
+        // Generate some text
+        String message = RandomStringUtils.randomAlphanumeric(level.random.nextInt(200));
+
+        // Send the message, with a fake packet
+        player.connection.handleChat(new ServerboundChatPacket(message));
+    }
+}
---
+++ src/main/java/uk/gemwire/keyboards/Mod.java
@@ -0,0 +1,26 @@
+package uk.gemwire.keyboards;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmllegacy.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+@net.minecraftforge.fml.common.Mod(Mod.MODID)
+public class Mod {
+    public static final String MODID = "keyboards";
+
+    private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+
+    private static final RegistryObject<Block> KEYBOARD = BLOCKS.register("keyboard", Keyboard::new);
+    private static final RegistryObject<Item> KEYBOARD_ITEM = ITEMS.register("keyboard", () -> new BlockItem(KEYBOARD.get(), new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public Mod() {
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+}
